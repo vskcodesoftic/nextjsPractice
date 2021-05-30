1e230276f31e186c77e9725bef5e5bd7285712ad
vskcodesoftic <vegisiva@codesoftic.com>
2021-05-30 10:54:25 +0530

    added  a programatic navigationfor filtred data

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,12 +1,44 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import EventList from '../../components/events/EventList';
+import { getFilteredEvents } from '../../DummyData';
 
 function FilteredEvents() {
    const router = useRouter();
    console.log(router.query);
+
+   const FilterData = router.query.slug;
+
+   if(!FilterData){
+       return <p className="center">Loading...</p>
+   }
+
+   const FilteredYear = FilterData[0];
+   const FilteredMonth = FilterData[1];
+  
+   //string to num
+   const numYear = +FilteredYear;
+   const numMonth = +FilteredMonth;
+
+   // if numyear is nont a number or following condition is true then router is  not valid
+   if(isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear <2021 ||  numMonth <1 || numMonth > 12){
+       return <p>Please adjust the filter , invalid filter</p>
+   }
+
+  const FilteredEvents = getFilteredEvents({
+      year : numYear,
+      month : numMonth
+  })
+
+  if(!FilteredEvents || FilteredEvents.length === 0) {
+      return <p>no events found !</p>
+  }
+
+
+
     return (
         <div>
-            <h1>FiltredEvent posts</h1>
+            <EventList events={FilteredEvents} />
         </div>
     )
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,21 @@
+import { useRouter } from 'next/router'
 import React, { Fragment } from 'react'
 import EventList from '../../components/events/EventList'
 import EventsSearch from '../../components/events/EventSearch'
 import { getAllEvents } from '../../DummyData'
 
 function AllEventsPage() {
-    const Events = getAllEvents()
+    const router = useRouter();
+    const Events = getAllEvents();
+    
+    const findEventsHandler = (year ,month) => {
+        const fullPath = `/events/${year}/${month}`;
+
+        router.push(fullPath)
+    }
     return (
         <Fragment>
-            <EventsSearch />
+            <EventsSearch onSearch={findEventsHandler} />
             <EventList events={Events} />
         </Fragment>
     )
